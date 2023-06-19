4e98b00e8858ceb9e107d34fdafe8a3cc23154e7
naritomo08 <naritomo08@gmail.com>
2023-06-19 11:39:08 +0900

    不要ファイル削除

--- front/src/components/ColoredMessage.jsx
+++
@@ -1,8 +0,0 @@
-export const ColoredMessage = (props) => {
-
-    const contentStyle = {
-        color: props.color,
-        fontSize: "20px"
-    };
-    return <p style={contentStyle}>{props.message}</p>
-};
